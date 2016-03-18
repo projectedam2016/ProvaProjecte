d4530711c96686066b922c46a96bd71721d5e240
projectedam2016 <projectedam2016@gmail.com>
2016-03-18 10:56:39 +0100

    Initial commit

---
+++ app/src/main/java/com/example/alumne/provaprojecte/OwnedActivity.java
@@ -0,0 +1,14 @@
+package com.example.alumne.provaprojecte;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class OwnedActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owned);
+    }
+
+}
